349255781b196ade809911b769606630a27bec71
原罪 <531362022@qq.com>
2021-02-18 23:59:48 +0800

    refactor: Replace mocha to jest

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # qoq-compress
 Compress for qoq based on [koa-compress](https://github.com/koajs/compress) that support gzip, deflate and brotli.
 
-**Notice:** You are required to use `node@10.16+` before you can use `brotli` encoding.
-
 [![License](https://img.shields.io/github/license/qoq-ts/qoq-compress)](https://github.com/qoq-ts/qoq-compress/blob/master/LICENSE)
 [![GitHub Workflow Status (branch)](https://img.shields.io/github/workflow/status/qoq-ts/qoq-compress/CI/master)](https://github.com/qoq-ts/qoq-compress/actions)
 [![Codecov](https://img.shields.io/codecov/c/github/qoq-ts/qoq-compress)](https://codecov.io/gh/qoq-ts/qoq-compress)
+[![npm](https://img.shields.io/npm/v/qoq-compress)](https://www.npmjs.com/package/qoq-compress)
 
 # Installation
 ```bash
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,105 +1,97 @@
-import { WebApplication, createWebRouter, WebSlotManager } from 'qoq';
+import { Server } from 'http';
+import { WebApplication, WebSlotManager, WebRouter } from 'qoq';
 import supertest from 'supertest';
 import { Compress } from '../src';
-import { expect } from 'chai';
 
-const nodeVersion = process.versions.node.split('.').map(Number);
-const hasBrotli = nodeVersion[0]! > 10 || nodeVersion[0] === 10 && nodeVersion[1]! >= 16;
+let app: WebApplication;
+let router: WebRouter;
+let listen: Server;
+let server: ReturnType<typeof supertest>;
+
+beforeEach(() => {
+  app = new WebApplication();
+  router = new WebRouter({
+    slots: new WebSlotManager(),
+  });
+  app.appendRoutes(router);
+  listen = app.listen();
+  server = supertest(listen);
+});
+
+afterEach(() => {
+  listen.close();
+});
 
 it ('brotli firstly', async () => {
-  const app = new WebApplication();
-  const router = createWebRouter(WebSlotManager);
-
   router.get('/').use(new Compress()).action((ctx) => {
     ctx.response.statusCode = 200;
     ctx.send('tt'.repeat(2000));
   });
 
-  app.appendRoutes(router);
-
-  await supertest(app.listen())
+  await server
     .get('/')
     .set('accept-encoding', 'gzip, deflate, br')
-    .expect('content-encoding', hasBrotli ? 'br': 'gzip');
+    .expect('content-encoding', 'br');
 });
 
 it ('gzip for wildcard', async () => {
-  const app = new WebApplication();
-  const router = createWebRouter(WebSlotManager);
-
   router.get('/').use(new Compress()).action((ctx) => {
     ctx.response.statusCode = 200;
     ctx.send('tt'.repeat(2000));
   });
   app.appendRoutes(router);
 
-  await supertest(app.listen())
+  await server
     .get('/')
     .set('accept-encoding', '*')
     .expect('content-encoding', 'gzip');
 });
 
 it ('no compression for small bytes', async () => {
-  const app = new WebApplication();
-  const router = createWebRouter(WebSlotManager);
-
   router.get('/').use(new Compress()).action((ctx) => {
     ctx.response.statusCode = 200;
     ctx.send('tt');
   });
-  app.appendRoutes(router);
 
-  await supertest(app.listen())
+  await server
     .get('/')
     .set('accept-encoding', '*')
     .then((res) => {
-      expect(res.header['content-encoding']).to.equal(undefined);
+      expect(res.header['content-encoding']).toEqual(undefined);
     });
 });
 
 it ('force compress for small bytes', async () => {
-  const app = new WebApplication();
-  const router = createWebRouter(WebSlotManager);
-
   router.get('/').use(new Compress({ threshold: 0 })).action((ctx) => {
     ctx.response.statusCode = 200;
     ctx.send('ttt');
   });
-  app.appendRoutes(router);
 
-  await supertest(app.listen())
+  await server
     .get('/')
     .set('accept-encoding', 'gzip, deflate')
     .expect('content-encoding', 'gzip');
 });
 
 it ('can set bytes', async () => {
-  const app = new WebApplication();
-  const router = createWebRouter(WebSlotManager);
-
   router.get('/').use(new Compress({ threshold: 3 })).action((ctx) => {
     ctx.response.statusCode = 200;
     ctx.send('tttt');
   });
-  app.appendRoutes(router);
 
-  await supertest(app.listen())
+  await server
     .get('/')
     .set('accept-encoding', 'gzip, deflate')
     .expect('content-encoding', 'gzip');
 });
 
 it ('can disable kind of them', async () => {
-  const app = new WebApplication();
-  const router = createWebRouter(WebSlotManager);
-
   router.get('/').use(new Compress({ br: false })).action((ctx) => {
     ctx.response.statusCode = 200;
     ctx.send('tt'.repeat(2000));
   });
-  app.appendRoutes(router);
 
-  await supertest(app.listen())
+  await server
     .get('/')
     .set('accept-encoding', 'gzip, deflate, br')
     .expect('content-encoding', 'gzip');
